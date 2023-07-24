77fa87bc09274bc52897e036f3526d626f19d196
Dmitry <dmitri.marchenko96@yandex.ru>
2023-07-24 23:37:49 +0500

    first commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,53 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class FilmController {
+    private int id = 0;
+    Map<Integer, Film> films = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
+
+    @GetMapping("/films")
+    public List<Film> findAll() {
+        if (films.equals(null))
+            throw new ValidationException("Нет фильмов", HttpStatus.NO_CONTENT);
+        return films.values().stream().toList();
+    }
+
+    @PostMapping("/films")
+    public Film create(@RequestBody Film film) {
+        if (film.getName().equals("") || film.getName().isEmpty())
+            throw new ValidationException("название не может быть пустым", HttpStatus.NOT_FOUND);
+        if (film.getDescription().length() > 200)
+            throw new ValidationException("максимальная длина описания — 200 символов", HttpStatus.BAD_REQUEST);
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895,12,1)))
+            throw new ValidationException("дата релиза — не раньше 28 декабря 1895 года", HttpStatus.BAD_REQUEST);
+        if (film.getDuration() < 0)
+            throw new ValidationException("продолжительность фильма должна быть положительной", HttpStatus.BAD_REQUEST);
+        film.setId(++id);
+        this.id = film.getId();
+        films.put(film.getId(), film);
+        return film;
+    }
+    @PutMapping("/films")
+    public Film update(@RequestBody Film film){
+
+        if (films.containsKey(film.getId())){
+            films.put(film.getId(), film);
+        }
+        else throw new ValidationException("Нет такого пользователя", HttpStatus.NOT_FOUND);
+        return film;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    protected int id = 0;
+    Map<Integer, User> users = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/users")
+    public List<User> findAll() {
+        if (users.entrySet().size() == 0)
+            throw new ValidationException("Нет пользователей", HttpStatus.NOT_FOUND);
+        return users.values().stream().toList();
+    }
+
+    @PostMapping("/users")
+    public User create(@RequestBody User user) {
+        if ((user.getLogin().contains(" ")) || (user.getLogin().equals(null)))
+            throw new ValidationException("логин не может быть пустым и содержать пробелы", HttpStatus.BAD_REQUEST);
+        if ((!user.getEmail().contains("@")) || (user.getEmail().equals(null)))
+            throw new ValidationException("электронная почта не может быть пустой и должна содержать символ @", HttpStatus.BAD_REQUEST);
+        if (user.getBirthday().isAfter(LocalDate.now()))
+            throw new ValidationException("дата рождения не может быть в будущем.", HttpStatus.BAD_REQUEST);
+        if (Optional.ofNullable(user.getName()).isEmpty())
+            user.setName(user.getLogin());
+        user.setId(++id);
+        this.id = user.getId();
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping("/users")
+    public User update(@RequestBody User user){
+
+        if (users.containsKey(user.getId())){
+            users.put(user.getId(), user);
+        }
+
+        else throw new ValidationException("Нет такого пользователя", HttpStatus.NOT_FOUND);
+        return user;
+    }
+}
